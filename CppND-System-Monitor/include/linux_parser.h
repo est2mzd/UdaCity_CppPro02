ffdb936b7b8e432482fa1fe256adfdf1389a25cd
est2mzd <est2mzd@gmail.com>
2020-07-06 11:35:26 +0900

    test

--- CppND-System-Monitor/include/linux_parser.h
+++ CppND-System-Monitor/include/linux_parser.h
@@ -49,8 +49,8 @@ long IdleJiffies();
 
 // Processes
 std::string Command(int pid);
-std::string Ram(int pid);
-std::string Uid(int pid);
+float Ram(int pid);
+int Uid(int pid);
 std::string User(int pid);
 long int UpTime(int pid);
 };  // namespace LinuxParser
